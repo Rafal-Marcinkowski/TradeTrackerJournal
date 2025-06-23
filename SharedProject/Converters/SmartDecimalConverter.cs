fd8d54956ff55126530519babc0be4100dcb0b0f
Rafał Marcinkowski <rafal.marcinkowski94@gmail.com>
2025-06-23 12:37:45 +0200

    "Smart"DecimalConverter for formatting dailydata

---
+++ SharedProject/Converters/SmartDecimalConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace SharedProject.Converters;
+
+public class SmartDecimalConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value == null)
+            return "";
+
+        if (value is decimal d)
+            return d.ToString("0.###", culture);
+
+        return "";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotImplementedException();
+}
